cdf23f7d64193a80bf7ba2ac8b4c1cac8b0a8512
Matheus <matheus.santos2@zup.com>
2020-09-28 11:13:26 -0300

    Project created

---
+++ src/test/java/com/casadocodigo/cadaDoCodigo/CadaDoCodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casadocodigo.cadaDoCodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadaDoCodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

63c881ddd478d0c1ab633fac52c0983c1fbeb5f1
Vinicius Nunes Moura <vinicnm@gmail.com>
2020-09-29 17:01:54 -0300

    Inclusão do start spring

---
+++ src/main/java/com/itau/cdc/Bootcamp01TemplateCasaDoCodigoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootcamp01TemplateCasaDoCodigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bootcamp01TemplateCasaDoCodigoApplication.class, args);
+	}
+
+}

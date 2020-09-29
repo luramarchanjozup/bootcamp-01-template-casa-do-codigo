9af09f312121b325fcd59ff5b40c9522b0afdfcc
Marcos Couto <marcos.couto@zup.com.br>
2020-09-29 10:37:35 -0300

    Exibir lista de livros

--- src/main/java/com/github/marcoscoutozup/casadocodigo/livro/LivroController.java
+++ src/main/java/com/github/marcoscoutozup/casadocodigo/livro/LivroController.java
@@ -2,15 +2,14 @@ package com.github.marcoscoutozup.casadocodigo.livro;
 
 import com.github.marcoscoutozup.casadocodigo.cadastroautor.Autor;
 import com.github.marcoscoutozup.casadocodigo.cadastrocategoria.Categoria;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/livro")
@@ -21,14 +20,23 @@ public class LivroController {
 
     @PostMapping
     @Transactional
-    public Livro cadastraLivro(@RequestBody @Valid LivroDTO dto){
+    public Livro cadastrarLivro(@RequestBody @Valid LivroDTO dto){
         Autor autor = entityManager.find(Autor.class, dto.getAutor());
         Categoria categoria = entityManager.find(Categoria.class, dto.getCategoria());
+
         Livro livro = dto.toModel();
         livro.setAutor(autor);
         livro.setCategoria(categoria);
+
         entityManager.persist(livro);
         return livro;
+    }
+
+    @GetMapping
+    public List<Livro> listarLivros(){
+        Query query = entityManager.createQuery("select l from Livro l", Livro.class);
+        List<Livro> livros = query.getResultList();
+        return livros;
     }
 
 }

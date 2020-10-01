0c433a52284f3d100b5f8f67888ddc475cdeae5f
Fernanda Vedovello <fe.vedovello@gmail.com>
2020-10-01 09:33:15 -0300

    Primeiro commit

---
+++ src/main/java/br/com/zup/treinocasadocodigo/TreinoCasaDoCodigoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.treinocasadocodigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinoCasaDoCodigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinoCasaDoCodigoApplication.class, args);
+	}
+
+}

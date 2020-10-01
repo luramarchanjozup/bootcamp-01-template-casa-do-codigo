4a5efec66480461e07e276cd94d3b5fed92db141
CarlosEduardo12 <carlosdudujunior@hotmail.com>
2020-10-01 19:50:32 -0300

    Adicionando Consulta por email

--- casadocodigo/src/main/java/br/com/carlos/casadocodigo/domain/repository/AutorRepository.java
+++ casadocodigo/src/main/java/br/com/carlos/casadocodigo/domain/repository/AutorRepository.java
@@ -4,6 +4,9 @@ import br.com.carlos.casadocodigo.domain.entity.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AutorRepository extends JpaRepository<Autor, Long> {
+    Optional<Autor> findByEmail(String email);
 }

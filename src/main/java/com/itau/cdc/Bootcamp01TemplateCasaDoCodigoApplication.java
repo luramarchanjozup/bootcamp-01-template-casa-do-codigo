3c66e1ce9ad383b0c9f1078a5c94d6fefd7e3526
Vinicius Nunes Moura <vinicnm@gmail.com>
2020-10-01 17:35:20 -0300

    Incluir Autor MySQL

--- src/main/java/com/itau/cdc/Bootcamp01TemplateCasaDoCodigoApplication.java
+++ src/main/java/com/itau/cdc/Bootcamp01TemplateCasaDoCodigoApplication.java
@@ -2,9 +2,18 @@ package com.itau.cdc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class Bootcamp01TemplateCasaDoCodigoApplication {
+@ComponentScan("com.itau.cdc")
+public class Bootcamp01TemplateCasaDoCodigoApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Bootcamp01TemplateCasaDoCodigoApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Bootcamp01TemplateCasaDoCodigoApplication.class, args);
--- src/test/java/com/itau/cdc/Bootcamp01TemplateCasaDoCodigoApplicationTest.java
+++ src/test/java/com/itau/cdc/Bootcamp01TemplateCasaDoCodigoApplicationTest.java
@@ -1,14 +1,16 @@
 package com.itau.cdc;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 class Bootcamp01TemplateCasaDoCodigoApplicationTest {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
 	}
 
 }

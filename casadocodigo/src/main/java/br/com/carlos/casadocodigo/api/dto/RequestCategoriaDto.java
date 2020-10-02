7ae4dd9aaccc075b7cc25880e3115aabf9eda8aa
CarlosEduardo12 <carlosdudujunior@hotmail.com>
2020-10-02 14:53:33 -0300

    DTO categorias

---
+++ casadocodigo/src/main/java/br/com/carlos/casadocodigo/api/dto/RequestCategoriaDto.java
@@ -0,0 +1,15 @@
+package br.com.carlos.casadocodigo.api.dto;
+
+import br.com.carlos.casadocodigo.api.handler.Unique;
+import br.com.carlos.casadocodigo.domain.entity.Categoria;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter @Setter
+public class RequestCategoriaDto {
+
+    @NotBlank @Unique(domainClass = Categoria.class, fieldName = "nome")
+    private String nome;
+}

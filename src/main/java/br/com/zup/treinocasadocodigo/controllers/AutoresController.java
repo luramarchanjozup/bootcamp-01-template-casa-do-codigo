8fdb6ad14e2929380b7d273a978e3d69ac9c2d5e
Fernanda Vedovello <fe.vedovello@gmail.com>
2020-10-02 12:21:33 -0300

    Verificar e-mail repetido

--- src/main/java/br/com/zup/treinocasadocodigo/controllers/AutoresController.java
+++ src/main/java/br/com/zup/treinocasadocodigo/controllers/AutoresController.java
@@ -2,17 +2,23 @@ package br.com.zup.treinocasadocodigo.controllers;
 
 import br.com.zup.treinocasadocodigo.entities.Autor;
 import br.com.zup.treinocasadocodigo.entities.NovoAutorRequest;
+import br.com.zup.treinocasadocodigo.repository.AutorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.servlet.http.HttpServletResponse;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
+import java.util.Optional;
 
 /**
- * Contagem de carga intrínseca da classe: 2
+ * Contagem de carga intrínseca da classe: 4
  */
 
 @RestController
@@ -21,12 +27,25 @@ public class AutoresController {
     @PersistenceContext
     private EntityManager manager;
 
+    @Autowired
+    //1
+    private AutorRepository autorRepository;
+
     @PostMapping(value="/autores")
     @Transactional
     //1
-    public String cadastroAutor(@RequestBody @Valid NovoAutorRequest novoAutor) {
+    public String cadastroAutor(@RequestBody @Valid NovoAutorRequest novoAutor, HttpServletResponse res) {
         //1
         Autor autor = novoAutor.toModel();
+
+        Optional<Autor> autorTeste = autorRepository.findByEmail(autor.getEmail());
+
+        //1
+        if(autorTeste.isPresent()) {
+            res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return "E-mail já está cadastrado";
+        }
+
         manager.persist(autor);
         return autor.toString();
     }
--- src/main/java/br/com/zup/treinocasadocodigo/repository/AutorRepository.java
+++ src/main/java/br/com/zup/treinocasadocodigo/repository/AutorRepository.java
@@ -4,11 +4,15 @@ import br.com.zup.treinocasadocodigo.entities.Autor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 /**
  * Contagem de carga intrínseca da classe: 0
  */
 
 @Repository
 public interface AutorRepository extends CrudRepository<Autor, Long> {
+
+    Optional<Autor> findByEmail(String email);
 
 }

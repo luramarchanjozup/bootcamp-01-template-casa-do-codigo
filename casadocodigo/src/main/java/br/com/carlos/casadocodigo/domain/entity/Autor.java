26591ef8ed0feac4bbe9006d00978229aece031c
CarlosEduardo12 <carlosdudujunior@hotmail.com>
2020-10-02 14:50:50 -0300

    Constraints para criar a entidade no banco

--- casadocodigo/src/main/java/br/com/carlos/casadocodigo/domain/entity/Autor.java
+++ casadocodigo/src/main/java/br/com/carlos/casadocodigo/domain/entity/Autor.java
@@ -7,19 +7,36 @@ import org.hibernate.annotations.CreationTimestamp;
 import javax.persistence.*;
 import java.util.Date;
 
-@Getter
-@Setter
 @Entity
-@Table(name = "autor")
+@Table(name = "autores")
 public class Autor {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Getter @Setter
+    @Column(nullable=false, length=100)
     private String nome;
+
+    @Getter @Setter
+    @Column(nullable=false, length=100, unique=true)
     private String email;
+
+    @Getter @Setter
+    @Column(nullable=false, length=400)
     private String descricao;
+
     @CreationTimestamp
     private Date timestamp;
+
+    @Deprecated
+    public Autor(){}
+
+    public Autor(String nome, String email, String descricao) {
+        this.nome = nome;
+        this.email = email;
+        this.descricao = descricao;
+    }
 
 }

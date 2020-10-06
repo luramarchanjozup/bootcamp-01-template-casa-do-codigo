2f302bb97f68fa67c96a19dde1f55119fc9d15ad
Matheus <matheus.santos2@zup.com>
2020-10-06 14:14:09 -0300

    PurchaseController refactoring

--- src/main/java/com/casadocodigo/casaDoCodigo/controllers/PurchasesController.java
+++ src/main/java/com/casadocodigo/casaDoCodigo/controllers/PurchasesController.java
@@ -4,20 +4,13 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
-import javax.validation.ValidationException;
 
-import com.casadocodigo.casaDoCodigo.controllers.dto.BookPurchaseDto;
 import com.casadocodigo.casaDoCodigo.controllers.dto.CartDetails;
+import com.casadocodigo.casaDoCodigo.controllers.dto.ClientCountry;
 import com.casadocodigo.casaDoCodigo.controllers.dto.PurchaseDto;
-import com.casadocodigo.casaDoCodigo.controllers.form.CartItemsForm;
 import com.casadocodigo.casaDoCodigo.controllers.form.PurchaseForm;
-import com.casadocodigo.casaDoCodigo.model.Book;
 import com.casadocodigo.casaDoCodigo.model.Cart;
-import com.casadocodigo.casaDoCodigo.model.CartItem;
-import com.casadocodigo.casaDoCodigo.model.Country;
-import com.casadocodigo.casaDoCodigo.model.Coupon;
 import com.casadocodigo.casaDoCodigo.model.Purchases;
-import com.casadocodigo.casaDoCodigo.model.State;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/purchase")
+//6
 public class PurchasesController {
 
     @PersistenceContext
@@ -35,69 +29,16 @@ public class PurchasesController {
     @PostMapping
     @Transactional
     public ResponseEntity<PurchaseDto> createPurchase(@RequestBody @Valid PurchaseForm form) {
-        CartDetails cartDetails = cartDetails(form);
+        CartDetails cartDetails = form.createCart(manager);
+        ClientCountry clientCountry = new ClientCountry(manager, form);
 
-        if (form.getCart().getTotal() != cartDetails.getQuantity()) {
-            throw new ValidationException("Cart total does not match the number of items");
-        }
-
-        Coupon coupon = manager.createQuery("SELECT c FROM Coupon c WHERE c.code = :code", Coupon.class)
-                        .setParameter("code", form.getCart().getCoupon()).getSingleResult();
-        
-        float discountedPrice = applyCoupon(form, cartDetails, coupon);
-        String appliedCoupon = couponMessage(form, coupon);
-
-        State state = manager.createQuery("SELECT s FROM State s WHERE s.name = :name", State.class)
-            .setParameter("name", form.getState()).getSingleResult();
-        Country country = manager.createQuery("SELECT c FROM Country c WHERE c.name = :name", Country.class)
-        .setParameter("name", form.getCountry()).getSingleResult();
-
-        Purchases purchase = new Purchases(form, state, country, new Cart(cartDetails.getQuantity(), cartDetails.getCartItems()));
+        Purchases purchase = new Purchases(form, clientCountry.getState(), clientCountry.getCountry(), 
+                            new Cart(cartDetails.getQuantity(), cartDetails.getCartItems()));
 
         manager.persist(purchase);
 
         return ResponseEntity.ok().body(
-            new PurchaseDto(purchase, cartDetails.getBooks(), cartDetails.getFinalPrice(), discountedPrice, appliedCoupon));
-    }
-
-    private CartDetails cartDetails(PurchaseForm form) {
-        CartDetails cartDetails = new CartDetails();
-
-        for (CartItemsForm book : form.getCart().getItems()) {
-            Book bookId = manager.find(Book.class, book.getBookId());
-            cartDetails.setBooks(new BookPurchaseDto(bookId));
-            cartDetails.setCartItems(new CartItem(book.getBookId(), book.getQuantity()));
-            cartDetails.setFinalPrice(cartDetails.getFinalPrice()
-                    + manager.find(Book.class, book.getBookId()).getPrice() * book.getQuantity());
-            cartDetails.setQuantity(cartDetails.getQuantity() + book.getQuantity());
-        }
-
-        return cartDetails;
-    }
-
-    private float applyCoupon(PurchaseForm form, CartDetails cart, Coupon coupon) {
-        float discountedPrice = cart.getFinalPrice();
-        
-        System.out.println("----------------------");
-        System.out.println(coupon);
-        System.out.println("----------------------");
-
-        if (!form.getCart().getCoupon().isBlank() && coupon != null) {
-            discountedPrice = cart.getFinalPrice() - (cart.getFinalPrice() * coupon.getPercentage());
-        }
-
-        return discountedPrice;
-    }
-
-    private String couponMessage(PurchaseForm form, Coupon coupon) {
-        String appliedCoupon = "No coupons applied";
-
-        if(coupon != null) {
-            appliedCoupon = "Coupon code " + form.getCart().getCoupon() + " with a discount of "
-                + (coupon.getPercentage() * 100) + "% has been applied";
-        } else if (!form.getCart().getCoupon().isBlank()) {
-            appliedCoupon = "No coupon of code " + form.getCart().getCoupon() + " found. No discount applied";
-        }
-        return appliedCoupon;
+            new PurchaseDto(purchase, cartDetails.getBooks(), cartDetails.getFinalPrice(), 
+                            cartDetails.getDiscountedPrice(), cartDetails.getAppliedCoupon()));
     }
 }
--- src/main/java/com/casadocodigo/casaDoCodigo/controllers/dto/CartDetails.java
+++ src/main/java/com/casadocodigo/casaDoCodigo/controllers/dto/CartDetails.java
@@ -3,19 +3,27 @@ package com.casadocodigo.casaDoCodigo.controllers.dto;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.casadocodigo.casaDoCodigo.controllers.form.CartItemsForm;
+import com.casadocodigo.casaDoCodigo.model.Book;
 import com.casadocodigo.casaDoCodigo.model.CartItem;
+import com.casadocodigo.casaDoCodigo.model.Coupon;
 
 public class CartDetails {
     private List<BookPurchaseDto> books;
     private List<CartItem> cartItems;
+    private Coupon coupon;
     private Float finalPrice;
+    private Float discountedPrice;
     private Integer quantity;
+    private String appliedCoupon;
 
     public CartDetails() {
         this.books = new ArrayList<>();
         this.cartItems = new ArrayList<>();
         this.finalPrice = 0f;
+        this.discountedPrice = 0f;
         this.quantity = 0;
+        this.appliedCoupon = "No coupons applied";
     }
 
     public List<BookPurchaseDto> getBooks() {
@@ -38,15 +46,48 @@ public class CartDetails {
         return this.finalPrice;
     }
 
-    public void setFinalPrice(Float finalPrice) {
-        this.finalPrice = finalPrice;
+    public void setFinalPrice(Book bookId, CartItemsForm cartBook) {
+        this.finalPrice = this.finalPrice + bookId.getPrice() * cartBook.getQuantity();
     }
 
     public Integer getQuantity() {
         return this.quantity;
     }
 
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
+    public void setQuantity(CartItemsForm book) {
+        this.quantity = this.quantity + book.getQuantity();
     }
+
+    public Coupon getCoupon() {
+        return this.coupon;
+    }
+
+    public void setCoupon(Coupon coupon) {
+        this.coupon = coupon;
+    }
+
+    public Float getDiscountedPrice() {
+        return this.discountedPrice;
+    }
+
+    public void setDiscountedPrice() {
+        this.discountedPrice = this.finalPrice;
+        if(this.getCoupon() != null) {
+            this.discountedPrice = this.finalPrice - (this.finalPrice * this.coupon.getPercentage());
+        }
+    }
+
+    public String getAppliedCoupon() {
+        return this.appliedCoupon;
+    }
+
+    public void setAppliedCoupon(String formCoupon) {
+        if(this.coupon != null) {
+            this.appliedCoupon = "Coupon code " + this.coupon.getCode() + " with a discount of "
+            + (this.coupon.getPercentage() * 100) + "% has been applied";
+        } else if (!formCoupon.isBlank()) {
+            this.appliedCoupon = "No coupon of code " + formCoupon + " found. No discount applied";
+        }
+    }
+
 }

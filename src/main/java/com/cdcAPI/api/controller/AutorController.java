df56e2a13234f4bbba913e3b4203ba6c92e57b2b
thaismathias <thais-mathias@hotmai.com>
2020-10-06 10:48:54 -0300

    Retirando exceção
    Excluindo classe e alterando controller para retirar exceção EmailDuplicado, que foi substituída pela annotation EntradaUnica

--- src/main/java/com/cdcAPI/api/controller/AutorController.java
+++ src/main/java/com/cdcAPI/api/controller/AutorController.java
@@ -1,35 +1,26 @@
 package com.cdcAPI.api.controller;
 
-import com.cdcAPI.api.exception.ExceptionEmailDuplicado;
 import com.cdcAPI.api.model.AutorRequest;
 import com.cdcAPI.model.Autor;
 import com.cdcAPI.repository.AutorRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
+
+//Complexidade
+//AutorRequest, Autor, AutorRepository
+//Total = 3
 
 @RestController
 public class AutorController {
 
     @Autowired
     private AutorRepository autorRepository;
-
-//    @Autowired // Validar autor por email único
-//    private ExceptionEmailDuplicado exceptionEmailDuplicado;
-//
-//    //Corte transversal para config validador
-//    @InitBinder
-//    public void init(WebDataBinder binder) {
-//        //1
-//        binder.addValidators(exceptionEmailDuplicado); //Add validação
-//    }
 
     //Criar autor
     @PostMapping("/autores")

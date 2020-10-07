a6fa110a2ab4a9b5544ff8b93019ff79a86016c8
jonathassk <jonathas09@gmail.com>
2020-10-07 12:25:07 -0300

    listar livros funcionando

--- src/main/java/com/bootcamp/cdd/controllers/BookController.java
+++ src/main/java/com/bootcamp/cdd/controllers/BookController.java
@@ -2,15 +2,15 @@ package com.bootcamp.cdd.controllers;
 
 import com.bootcamp.cdd.models.Book;
 import com.bootcamp.cdd.request.BookRequest;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/v1/book")
@@ -19,10 +19,18 @@ public class BookController {
     private EntityManager entityManager;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     @Transactional
     public Book createBook (@RequestBody @Valid BookRequest request) {
         Book book = request.toModel(entityManager);
         entityManager.persist(book);
         return book;
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<?> listBooks () {
+        Query query = entityManager.createQuery("SELECT b FROM Book b", Book.class);
+        return query.getResultList();
     }
 }
--- src/main/java/com/bootcamp/cdd/request/BookRequest.java
+++ src/main/java/com/bootcamp/cdd/request/BookRequest.java
@@ -4,6 +4,7 @@ import com.bootcamp.cdd.models.Author;
 import com.bootcamp.cdd.models.Book;
 import com.bootcamp.cdd.models.Category;
 import com.bootcamp.cdd.shared.UniqueValue;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.util.Assert;
 
@@ -16,21 +17,21 @@ import java.time.LocalDate;
 public class BookRequest {
     @NotBlank(message = "Titulo é obrigatório")
     @UniqueValue(domainClass = Book.class, fieldName = "titulo", message = "Esse titulo já foi cadastrado")
-    private String titulo;
-    private String sumario;
+    private final String titulo;
+    private final String sumario;
     @Min(value = 100, message = "O livro deve ter pelo menos 100 paginas")
-    private int quantidadePaginas;
+    private final int quantidadePaginas;
     @NotBlank(message = "Resumo é obrigatório") @Length(min = 1, max = 500, message = "Sumario deve ter entre 1 e 500 caracteres.")
-    private String resumo;
+    private final String resumo;
     @Min(value = 20, message = "Preço deve ser maior que 20.")
-    private double preco;
+    private final double preco;
     @NotBlank(message = "Isnb é obrigatório")
     @UniqueValue(domainClass = Book.class, fieldName = "isnb", message = "Esse isnb já foi cadastrado")
-    private String isnb;
+    private final String isnb;
     @Future
-    private LocalDate dataPublicacao;
-    private long categoryId;
-    private long authorId;
+    private final LocalDate dataPublicacao;
+    private final long categoryId;
+    private final long authorId;
 
 
     public BookRequest(@NotBlank(message = "Titulo é obrigatório") String titulo, String sumario, int quantidadePaginas, @NotBlank(message = "Resumo é obrigatório") @Length(min = 1, max = 500, message = "Sumario deve ter entre 1 e 500 caracteres.") String resumo, @Min(value = 20, message = "Preço deve ser maior que 20.") double preco, @NotBlank(message = "Isnb é obrigatório") String isnb, LocalDate dataPublicacao, long categoryId, long authorId) {

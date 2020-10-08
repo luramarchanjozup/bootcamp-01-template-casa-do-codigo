50645e912fde62103ed425605296527c61491c22
CarlosEduardo12 <carlosdudujunior@hotmail.com>
2020-10-08 08:47:39 -0300

    removendo classes desnecessarias

--- casadocodigo/src/main/java/br/com/carlos/casadocodigo/domain/service/CadastroAutorService.java
+++
@@ -1,7 +0,0 @@
-package br.com.carlos.casadocodigo.domain.service;
-
-import br.com.carlos.casadocodigo.domain.entity.Autor;
-
-public interface CadastroAutorService {
-    Autor salvar(Autor autor);
-}

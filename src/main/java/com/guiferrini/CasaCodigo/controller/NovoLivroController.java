aa8050811fd4cd3a550275d0b402bd4b689e4765
guiferrini <guilhermemferrini@gmail.com>
2020-10-12 22:24:50 -0300

    Adicionado rota novolivro - get - lista de todos os livros

--- src/main/java/com/guiferrini/CasaCodigo/controller/NovoLivroController.java
+++ src/main/java/com/guiferrini/CasaCodigo/controller/NovoLivroController.java
@@ -1,11 +1,17 @@
 package com.guiferrini.CasaCodigo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,6 +39,30 @@ public class NovoLivroController {
 		} else {
 			return ResponseEntity.status(400).body(obj); 
 		}
+	}
+	
+	@GetMapping
+	@Transactional
+	public ResponseEntity<List<NovoLivro>> busca(){
 		
+		Query query = entityManager.createQuery("Select x from " + NovoLivro.class.getName() + " x");
+		
+		List<NovoLivro> list = new ArrayList<>();
+		
+		list.addAll(query.getResultList());
+		
+		if(list instanceof NovoLivro) {
+			return ResponseEntity.status(200).body(list);			
+		} else {
+			return ResponseEntity.status(500).body(list);
+		}	
 	}
 }
+
+
+
+
+
+
+
+

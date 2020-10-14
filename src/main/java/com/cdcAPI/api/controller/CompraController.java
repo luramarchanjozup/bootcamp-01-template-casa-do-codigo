c14d0108c587f9f215e5a5c14a33638fe9d06594
thaismathias <thais-mathias@hotmai.com>
2020-10-14 17:35:46 -0300

    Movendo validações
    Movendo validações de ClienteRequest para CompraController

--- src/main/java/com/cdcAPI/api/controller/CompraController.java
+++ src/main/java/com/cdcAPI/api/controller/CompraController.java
@@ -4,6 +4,7 @@ import com.cdcAPI.api.model.Request.CompraRequest;
 import com.cdcAPI.api.model.Response.CompraResponse;
 import com.cdcAPI.model.Compra;
 
+import com.cdcAPI.model.Estado;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,8 @@ import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 import java.net.URI;
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping("/compras")
@@ -23,7 +26,33 @@ public class CompraController {
     @PostMapping
     @Transactional
     public ResponseEntity<Void> comprar(@Valid @RequestBody CompraRequest compraRequest) throws Exception {
+
         Compra compra = compraRequest.toModel(manager);
+
+        if ((compra.getCliente().getDocumento().length()) != 11 && (compra.getCliente().getDocumento().length()) != 14) {
+            throw new Exception("Compra não pode ser efetuada. Documento inválido");
+        }
+
+        List<Estado> estadosList = manager
+                .createQuery("SELECT e from Estado e WHERE pais = :pais", Estado.class)
+                .setParameter("pais", compra.getCliente().getPais())
+                .getResultList();
+
+        if (compra.getCliente().getPais() == null) {
+            throw new Exception("Compra não pode ser efetuada. Pais não existe."); //se pais null
+        }
+        //se pais existe
+        else if (estadosList.size() > 0) { //e lista de estados existe
+            if (compra.getCliente().getEstado() == null) { //estado obrigatorio então
+                throw new Exception("Campo 'estado' obrigatório."); // se estado null
+            } else if (!(compra.getCliente().getEstado().getPais()).equals(compra.getCliente().getPais())) { //estado existe o idPais = pais
+                throw new Exception("Compra não pode ser efetuada. Este estado não pertence ao país selecionado.");
+            }
+        } else if (compra.getCliente().getEstado() != null){ //se lista vazia, não pode estado
+            throw new Exception("Compra não pode ser efetuada. Este país não possui estados.");
+        }
+
+
         manager.persist(compra);
 
         String urlCreated = "/compras/" + compra.getId();
--- src/main/java/com/cdcAPI/api/model/Request/ClienteRequest.java
+++ src/main/java/com/cdcAPI/api/model/Request/ClienteRequest.java
@@ -1,20 +1,17 @@
 package com.cdcAPI.api.model.Request;
 
-import com.cdcAPI.api.model.Response.EstadoResponse;
-import com.cdcAPI.api.model.Response.PaisResponse;
 import com.cdcAPI.model.Cliente;
 import com.cdcAPI.model.Cupom;
 import com.cdcAPI.model.Estado;
 import com.cdcAPI.model.Pais;
-import org.hibernate.criterion.CriteriaQuery;
 
 import javax.persistence.EntityManager;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import java.time.LocalDate;
-import java.util.List;
-import java.util.Map;
+
+//Complexidade = 4
+//Cliente, pais, estado, cupom
 
 public class ClienteRequest {
 
@@ -51,39 +48,18 @@ public class ClienteRequest {
     @NotBlank
     private String cep;
 
-    private Long cupomId;
+    private Long cupomId = 0L;
 
     public Cliente toModel(EntityManager manager) throws Exception {
 
-        if (documento.length() != 11 && documento.length() != 14) {
-            throw new Exception("Compra não pode ser efetuada. Documento inválido");
-        }
-
-        @NotBlank
         Pais pais = manager.find(Pais.class, paisId);
-        Estado estado = manager.find(Estado.class, estadoId);
-        List<Estado> estadosList = manager
-                .createQuery("SELECT e from Estado e WHERE paisId = :paisId", Estado.class)
-                .setParameter("paisId", paisId)
-                .getResultList();
 
-        if (pais == null) {
-            throw new Exception("Compra não pode ser efetuada. Pais não existe.");
-        } else if (estadosList.size() > 0) {
-            if (estado == null) {
-                throw new Exception("Campo 'estado' obrigatório.");
-            } else if (!estado.getPaisId().equals(paisId)) {
-                throw new Exception("Compra não pode ser efetuada. Este estado não pertence ao país selecionado.");
-            }
+        Estado estado = null;
+        if (estadoId != null && estadoId > 0) {
+            estado = manager.find(Estado.class, estadoId);
         }
 
         Cupom cupom = manager.find(Cupom.class, cupomId);
-        if (cupom == null) {
-            throw new Exception("Compra não pode ser efetuada. Cupom não existe.");
-        }
-        if (!cupom.getValidade().isAfter(LocalDate.now())) {
-            throw new Exception("Compra não pode ser efetuada. Cupom expirou.");
-        }
 
         return new Cliente(email, nome, sobrenome, documento, endereco, complemento,
                 cidade, pais, estado, telefone, cep, cupom);

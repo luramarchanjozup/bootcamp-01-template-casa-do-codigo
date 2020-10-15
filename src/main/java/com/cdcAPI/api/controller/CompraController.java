a433187ec9e0ce4c1510dbcb63a80c82ea6db47b
thaismathias <thais-mathias@hotmai.com>
2020-10-15 17:56:28 -0300

    Transferindo validação do controller para nova classe
    Nova classe ValidarEstadoPertencePais:
    -Valida se id do país é válido
    -Valida se campo estado é obrigatório
    -Valida se estado pertence a país
    -Valida se país possui estados

--- src/main/java/com/cdcAPI/api/controller/CompraController.java
+++ src/main/java/com/cdcAPI/api/controller/CompraController.java
@@ -3,19 +3,19 @@ package com.cdcAPI.api.controller;
 import com.cdcAPI.api.model.Request.CompraRequest;
 import com.cdcAPI.api.model.Response.CompraResponse;
 import com.cdcAPI.model.Compra;
+import com.cdcAPI.validator.ValidarEstadoPertencePais;
 
-import com.cdcAPI.model.Estado;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 import java.net.URI;
-import java.time.LocalDate;
-import java.util.List;
 
+//Complexidade = 6
 @RestController
 @RequestMapping("/compras")
 public class CompraController {
@@ -23,38 +23,26 @@ public class CompraController {
     @Autowired
     EntityManager manager;
 
+    @Autowired
+    //1
+    private ValidarEstadoPertencePais validarEstadoPertencePais;
+
+    @InitBinder
+    public void init(WebDataBinder binder) {
+        binder.addValidators(validarEstadoPertencePais);
+    }
+
     @PostMapping
     @Transactional
+    //2 Compra Request
     public ResponseEntity<Void> comprar(@Valid @RequestBody CompraRequest compraRequest) throws Exception {
-
+        //3 compra
         Compra compra = compraRequest.toModel(manager);
-
+        //4
         if ((compra.getCliente().getDocumento().length()) != 11 && (compra.getCliente().getDocumento().length()) != 14) {
             throw new Exception("Compra não pode ser efetuada. Documento inválido");
         }
-
-        List<Estado> estadosList = manager
-                .createQuery("SELECT e from Estado e WHERE pais = :pais", Estado.class)
-                .setParameter("pais", compra.getCliente().getPais())
-                .getResultList();
-
-        if (compra.getCliente().getPais() == null) {
-            throw new Exception("Compra não pode ser efetuada. Pais não existe."); //se pais null
-        }
-        //se pais existe
-        else if (estadosList.size() > 0) { //e lista de estados existe
-            if (compra.getCliente().getEstado() == null) { //estado obrigatorio então
-                throw new Exception("Campo 'estado' obrigatório."); // se estado null
-            } else if (!(compra.getCliente().getEstado().getPais()).equals(compra.getCliente().getPais())) { //estado existe o idPais = pais
-                throw new Exception("Compra não pode ser efetuada. Este estado não pertence ao país selecionado.");
-            }
-        } else if (compra.getCliente().getEstado() != null){ //se lista vazia, não pode estado
-            throw new Exception("Compra não pode ser efetuada. Este país não possui estados.");
-        }
-
-
         manager.persist(compra);
-
         String urlCreated = "/compras/" + compra.getId();
 
         return ResponseEntity.created(URI.create(urlCreated)).build();
@@ -62,10 +50,11 @@ public class CompraController {
 
     @GetMapping("/{compraId}")
     @Transactional
+    //5 Compra response
     public ResponseEntity<CompraResponse> detalhesCompra(@PathVariable Long compraId) throws Exception {
         Compra compra = manager.find(Compra.class, compraId);
+        //6
         if (compra == null) throw new Exception("Compra não existe.");
-
         CompraResponse compraResponse = new CompraResponse(compra);
 
         return ResponseEntity.ok(compraResponse);

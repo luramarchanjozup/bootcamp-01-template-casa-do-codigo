80442f8f02d2805c09ed342f58b145fbd0e04a5d
CarlosEduardo12 <carlosdudujunior@hotmail.com>
2020-10-15 00:13:38 -0300

    simplificando codigo com EntityManager

--- casadocodigo/src/main/java/br/com/carlos/casadocodigo/domain/repository/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.carlos.casadocodigo.domain.repository;
-
-import br.com.carlos.casadocodigo.domain.entity.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-}
--- casadocodigo/src/main/java/br/com/carlos/casadocodigo/domain/service/CadastrarLivrosService.java
+++
@@ -1,7 +0,0 @@
-package br.com.carlos.casadocodigo.domain.service;
-import br.com.carlos.casadocodigo.domain.entity.Livro;
-
-public interface CadastrarLivrosService {
-    Livro criar(Livro livro);
-    Livro builder(Livro livro);
-}

df0d74bf15f34977ff63a1aaff3592d220d404b4
Rui Braz <rui.braz.correia@gmail.com>
2020-10-16 17:29:28 -0300

    Criando o response de DetalhesDaCompra.

--- src/main/java/br/com/bootcamp/zup/braz/rui/bootcamp01templatecasadocodigo/response/DetalhesCompraResponse.java
+++ src/main/java/br/com/bootcamp/zup/braz/rui/bootcamp01templatecasadocodigo/response/DetalhesCompraResponse.java
@@ -1,6 +1,11 @@
 package br.com.bootcamp.zup.braz.rui.bootcamp01templatecasadocodigo.response;
 
+import br.com.bootcamp.zup.braz.rui.bootcamp01templatecasadocodigo.domain.Compra;
+import br.com.bootcamp.zup.braz.rui.bootcamp01templatecasadocodigo.domain.Pedido;
+
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
 
 public class DetalhesCompraResponse {
 
@@ -14,6 +19,38 @@ public class DetalhesCompraResponse {
     private String telefone;
     @NotBlank
     private String email;
+    @NotBlank
+    private String cep;
+    @NotBlank
+    private String endereco;
+    @NotBlank
+    private String complemento;
+    @NotBlank
+    private String cidade;
+    @NotBlank
+    private String pais;
+    private String estado;
+    @NotNull
+    private BigDecimal totalPedido;
+    @NotNull
+    private PedidoResponse pedidoResponse;
+    private String cupomAplicado;
+    private Integer percentualDesconto;
+    private BigDecimal totalComDesconto;
+
+    public DetalhesCompraResponse(Compra compra, Pedido pedido){
+        this.documento = compra.getDocumento();
+        this.nome = compra.getNome();
+        this.sobrenome = compra.getSobrenome();
+        this.telefone = compra.getTelefone();
+        this.email = compra.getEmail();
+        this.cep = compra.getCep();
+        this.endereco = compra.getEndereco();
+        this.complemento = compra.getComplemento();
+        this.cidade = compra.getCidade();
+        this.pais = compra.getPais().getNome();
+        this.totalPedido;
+    }
 
 
 

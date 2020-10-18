5482ee76447fcf4a329dd211a61116157baa12e8
Maria <maria.guerra@zup.com.br>
2020-10-18 18:42:58 -0300

    criei Rota para buscar livro pelo id

--- src/main/java/br/com/zup/casadocodigo/detalheLivro/DetalheController.java
+++ src/main/java/br/com/zup/casadocodigo/detalheLivro/DetalheController.java
@@ -9,6 +9,7 @@ import javax.persistence.PersistenceContext;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.zup.casadocodigo.livro.Livro;
@@ -19,7 +20,7 @@ public class DetalheController {
 	@PersistenceContext
 	private EntityManager bancoDados;
 
-	@GetMapping(value = "/livros/detalheLivros", produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(value = "/livro/detalheLivro/lista", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<DetalheLivroResponseDTO>> listaLivros() {
 		List<Livro> geraLivro = bancoDados.createQuery("select l from Livro l", Livro.class).getResultList();
 
@@ -32,4 +33,17 @@ public class DetalheController {
 		return ResponseEntity.ok(resultadoLivro);
 
 	}
+
+	@GetMapping(value = "/livro/detalheLivro/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<DetalheLivroResponseDTO> buscarDetalheLivros(@PathVariable("id") Long id) {
+		Livro livroBuscado = bancoDados.find(Livro.class, id);
+
+		if (livroBuscado == null) {
+			return ResponseEntity.notFound().build();
+		}
+
+		DetalheLivroResponseDTO detalheLivroResponseDTO = new DetalheLivroResponseDTO(livroBuscado);
+		return ResponseEntity.ok(detalheLivroResponseDTO);
+	}
+
 }
